b466e55aa92ba92969e2e57c7d136eb5170cd109
hadeaji <hadeaji@gmail.com>
2022-02-05 13:26:45 +0200

    first time uploading Unity Project

---
+++ Assets/Animations/animationScriptController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationScriptController : MonoBehaviour
+{
+
+    Animator animator;
+    float velocity = 0.0f;
+    public float acceleration = 0.1f;
+    public float deceleration = 0.5f;
+    int VelocityHash;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        VelocityHash = Animator.StringToHash("Velocity");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (forwardPressed && velocity < 0.5f)
+        {
+            velocity += Time.deltaTime * acceleration;
+        }
+        if (forwardPressed && runPressed && velocity < 1.0f)
+        {
+            velocity += Time.deltaTime * acceleration;
+        }
+
+        if (forwardPressed && !runPressed && velocity > 0.5f)
+        {
+            velocity -= Time.deltaTime * deceleration;
+        }
+
+        if (!forwardPressed && velocity > 0.0f)
+        {
+            velocity -= Time.deltaTime * deceleration;
+        }
+
+        if (!forwardPressed && velocity < 0.0f)
+        {
+            velocity = 0.0f;
+        }
+
+        animator.SetFloat(VelocityHash, velocity);
+    }
+}
